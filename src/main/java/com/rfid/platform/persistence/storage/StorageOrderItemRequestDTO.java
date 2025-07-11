050c4ab0e70125999faa0baa66f843e6e2b4d104
dsh1991629 <dsh1991629>
2025-07-11 21:54:29 +0800

    通知单数据

--- src/main/java/com/rfid/platform/persistence/storage/StorageOrderItemRequestDTO.java
+++ src/main/java/com/rfid/platform/persistence/storage/StorageOrderItemRequestDTO.java
@@ -2,25 +2,36 @@ package com.rfid.platform.persistence.storage;
 
 import java.io.Serializable;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "通知单详情数据传输对象")
 public class StorageOrderItemRequestDTO implements Serializable {
 
+    @Schema(description = "款式码")
     private String productCode;
 
+    @Schema(description = "产品名")
     private String productName;
 
+    @Schema(description = "尺寸")
     private String productSize;
 
+    @Schema(description = "颜色")
     private String productColor;
 
+    @Schema(description = "SKU")
     private String sku;
 
+    @Schema(description = "件数")
     private Integer quantity;
 
+    @Schema(description = "箱数量")
     private Integer boxCnt;
 
+    @Schema(description = "箱外码")
     private List<String> boxCodes;
 
 
--- src/main/java/com/rfid/platform/persistence/storage/StorageOrderRequestDTO.java
+++ src/main/java/com/rfid/platform/persistence/storage/StorageOrderRequestDTO.java
@@ -2,13 +2,18 @@ package com.rfid.platform.persistence.storage;
 
 import java.io.Serializable;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "通知单数据传输对象")
 public class StorageOrderRequestDTO implements Serializable {
 
+    @Schema(description = "通知单号码")
     private String orderNo;
 
+    @Schema(description = "通知单数据详情")
     private List<StorageOrderItemRequestDTO> items;
 
 }
